e1c5b2a45a4bbe18bd3bdce01356bc922e5fdbc5
Sean Gordon <nodrog.naes@gmail.com>
2021-07-07 18:07:52 -0400

    working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -126,7 +126,7 @@ const renderNoteList = async (notes) => {
   let noteListItems = [];
 
   // Returns HTML element with or without a delete button
-  const createLi = (text, delBtn = true) => {
+  const createLi = (text, delBtn = false) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
 
